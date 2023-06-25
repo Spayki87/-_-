6ff05f63792cf0f631bacd096a7a581814932be6
Spayki87 <136745298+Spayki87@users.noreply.github.com>
2023-06-25 19:09:27 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Контроль_выхода
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            while (true)
+            {
+                Console.WriteLine("Введите exit, чтобы выйти.");
+                input = Console.ReadLine();
+                if(input == "exit")
+                {
+                    break;
+                }
+            }
+            Console.WriteLine("Вы вышли из программы.");
+        }
+    }
+}
